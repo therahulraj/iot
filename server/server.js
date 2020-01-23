d85829497f1d53d52cc938ec007c0d4346191154
therahulraj <rahulraj16mar@gmail.com>
2020-01-23 17:11:13 +0530

    firt test

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+var port = process.env.PORT || 3000;
+var global_color = 'white';
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public', 'index.html'));
+    });
+
+
+app.post('/api/changeColor',urlencodedParser, (req, res) => {
+    console.log(req.body.color);
+    global_color = req.body.color;
+    console.log(global_color);
+    res.status(200).send(global_color);
+
+})
+
+app.get('/getCurrentColor', (req, res) => {
+    console.log(global_color);
+    console.log('get request is being called.');
+    res.send({
+        color: global_color
+    })
+})
+
+    app.listen(port, () => {
+        console.log(`the server is up on ${port}`);
+      })
